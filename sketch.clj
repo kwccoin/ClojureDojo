4bcc68060abde33dd05c5ce8d36e9a2ca7f073a2
Zeroed <eddie@white.network>
2012-08-12 23:31:59 +0200

    updating and studying

--- sketch.clj
+++ sketch.clj
@@ -39,3 +39,44 @@ user=> (map str '("sleep" "run" "jump") '(" well" " fast" " high"))
 (defn create_keys [] (map #(str "@" %) (range 0 10)))
 user=> (map list '( 1 2 3) '(100 200 300))
 ((1 100) (2 200) (3 300))
+
+user=> (let [r (range 0 10)] (map str (map #(str "@" %) r) r))
+(map str '("sleep" "jump" "run"))
+("@00" "@11" "@22" "@33" "@44" "@55" "@66" "@77" "@88" "@99")
+
+(let [r (range 0 10)] (map #(str "@" %) r))
+("@0" "@1" "@2" "@3" "@4" "@5" "@6" "@7" "@8" "@9")
+
+user=> (defn kys [] (let [r (range 0 10)] (map #(str "@" %) r)))
+#'user/kys
+user=> (map list (kys) (range 0 10))
+(("@0" 0) ("@1" 1) ("@2" 2) ("@3" 3) ("@4" 4) ("@5" 5) ("@6" 6) ("@7" 7) ("@8" 8) ("@9" 9))
+
+-------------------------
+clojure.core/doseq
+([seq-exprs & body])
+Macro
+  Repeatedly executes body (presumably for side-effects) with
+  bindings and filtering as provided by "for".  Does not retain
+  the head of the sequence. Returns nil.
+
+(defn kys [] (let [r (range 0 10)] (map #(str "@" %) r)))
+(map list (kys) (range 0 10))
+;=> (("@0" 0) ("@1" 1) ("@2" 2) ("@3" 3) ("@4" 4) ("@5" 5) ("@6" 6) ("@7" 7) ("@8" 8) ("@9" 9))
+
+(doseq [[x y] (map list (kys) (range 0 10))] (println (str x ":" y)))
+(doseq [[x y] (map list (kys) (range 0 10))] (assoc {} x y))
+
+(let [m {}] (doseq [[x y] (map list (kys) (range 0 10))] (when [m] (println (str x ":" y " in " m)))))
+
+user=> (let [m {}] (doseq [[x y] (map list (kys) (range 0 10)), m (assoc m x y)] (println m)))
+[@0 0]
+[@1 1]
+[@2 2]
+[@3 3]
+[@4 4]
+[@5 5]
+[@6 6]
+[@7 7]
+[@8 8]
+[@9 9]
