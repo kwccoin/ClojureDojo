cd446fd548b92ee94847c7a756d76e25e6a92e6c
Zeroed <eddie@white.network>
2012-08-20 12:06:08 +0200

    updating and studying

--- random-map.clj
+++ random-map.clj
@@ -5,21 +5,17 @@
 
 ; lol try: (filter #(re-find #"Case" %) (map #(.getName %) (seq (.getMethods String))))
 
-; Create a list of string, length 1
-(defn keyz "create the keyz" [] (map str (map char (range 65 91))))
+;(defn n-rand [n] (take n (repeatedly rand)))
 ; (map #(.toLowerCase %) (keyz))
 
-(defn n-rand [n] (take n (repeatedly rand)))
+;every 2-combination of letters
+;(for [x (keyz) y (keyz)] (map str x y)) 
 
-#_(
+;apply a prefix
+;(doseq [x ":" y (keyz)] (println (str x y)))
+
 (use '[clojure.contrib.math :only (round)])
-(let [h 10  
-      l (inc (round (/ h 26)))
-      v (take h (repeatedly rand))
-      k (take h (keyz))] 
-      (zipmap k v)
-))
-
+(defn keyz "create the keyz" [] (map str (map char (range 65 91))))
 (defn foo [x] 
   (loop [times (dec x)
          k (keyz)
@@ -28,14 +24,13 @@
       s
       (recur (dec times) k (for [x (keyz) y s] (map str x y)))
 )))
-
-(foo 3)
-
-;every 2-combination of letters
-;(for [x (keyz) y (keyz)] (map str x y)) 
-
-;apply a prefix
-;(doseq [x ":" y (keyz)] (println (str x y)))
-
-
+(defn rand-map
+    ([] (rand-map 10)) 
+    ([size] (let [
+      h size  
+      length (inc (round (/ h 26)))
+      v (take h (repeatedly rand))
+      k (map str (map first (take h (foo length))))] 
+      (zipmap k v))
+))
 
