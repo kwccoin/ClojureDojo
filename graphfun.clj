860c601b38ab4dc963a9641986b2da0701f535dc
Zeroed <eddie@white.network>
2012-08-30 10:49:21 +0200

    updating and studying

---
+++ graphfun.clj
@@ -0,0 +1,22 @@
+(def frame (java.awt.Frame.))
+(.setVisible frame true)
+(.setSize frame (java.awt.Dimension. 200 200))
+
+(def gfx (.getGraphics frame))
+(.fillRect gfx 100 100 50 75)
+
+(defn f-values [f xs ys] 
+  (for [x (range xs) y (range ys)] 
+    [x y (rem (f x y) 256)]))
+
+(defn clear [g] 
+  (.clearRect g 0 0 200 200))
+
+(defn draw-values [f xs ys]
+  (clear gfx)
+  (.setSize frame (java.awt.Dimension. xs ys))
+  (doseq [[x y v] (f-values f xs ys)]
+    (.setColor gfx (java.awt.Color. v v v))
+    (.fillRect gfx x y 1 1)))
+
+(draw-values bit-and 256 256)
