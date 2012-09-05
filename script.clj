a5fadc0e5c6631b0b44c9bedae8f623b77d3859e
Zeroed <eddie@white.network>
2012-09-05 23:12:47 +0200

    updating and studying

--- script.clj
+++ script.clj
@@ -1,19 +1,32 @@
+; Let's try to parse a webpage using destructuring in a smart way.
+
+; the library
 (use 'pl.danieljanus.tagsoup)
+
+; the page
 (def foo (parse "http://www.example.com"))
-(defn tri [t] (let [[a b & c] t] [a b (vec c)]))
+
+; the destructure functions
+(defn tri-vec [parsed-page] (let [[tag property-map & more :as all] parsed-page] [tag property-map (vec more) all]))
+(defn tri-map [parsed-props] (let [[tag property-map & more :as all] parsed-props] {:tag tag :property-map property-map :more (vec more) :all all}))
+
+; the parser function
 (defn parz [src] 
-  (let [ttt (tri src)]
-    (if (ttt 2)
-      (if (keyword? (ttt 0))
-        (map parz (ttt 2))
-        (if (= (ttt 0) ":a")
-          (ttt 1) 
+  (let [ttt (tri-vec src)]
+    (let [tag (ttt 0) property-map (ttt 1) more (ttt 2)] 
+      (if more
+        (if (keyword? tag )
+          (if (= tag (keyword "a"))
+            (property-map :href)
+            (map parz more)
+          )
           (apply str src)
         )
       )
     )
   )
 )
+(parz foo )
 (defn clea [tst]
   (let [[h p & o] tst,
         c (count o)
